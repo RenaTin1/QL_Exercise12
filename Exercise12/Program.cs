7a55b6f064008f8c5995573d7a2de7992c1ea895
RenaTin1 <31945448+RenaTin1@users.noreply.github.com>
2017-10-04 20:19:52 -0400

    Exercise 12 (First Commit)

---
+++ Exercise12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Serena Tindle- Exercise 12 (counting down)
+            //variables and while loop
+            string UserOption;
+            bool ContinueProgram = true;
+            while (ContinueProgram == true)
+            {
+                for (int i = 10-1; i >=0; i--)  //for loop until i equals 0
+                {
+                    Console.Write(i);
+                }
+
+                Console.WriteLine(); //skip to next line
+                Console.WriteLine("Would you like to continue? (y/n)"); //Ask user if they want to continue
+                UserOption = Console.ReadLine();
+                if (UserOption == "N" || UserOption == "No" || UserOption == "no")
+                    ContinueProgram = false;//exit program
+            }
+
+
+
+        }
+    }
+}
